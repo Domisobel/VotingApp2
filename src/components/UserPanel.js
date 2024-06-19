f510be54102bebdde2d40227c8484c958a52279d
Dominika <pimmortals@gmail.com>
2024-06-19 13:36:29 +0200

    delet account- modal

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,11 +1,35 @@
-import "./UserPanel.css"; 
-import React, { useState } from "react";
+import "./UserPanel.css";
+import React, { useState, useEffect } from "react";
 
 const UserPanel = () => {
   const [activeButton, setActiveButton] = useState("user-info");
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape" || e.key === "Shift") {
+      if (showModal) {
+        closeModal();
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [showModal]);
 
   return (
-    <div className="container mt-3 mb-3 mt-sm-5 mb-sm-5 custom-container ">
+    <div className="container mt-3 mb-3 mt-sm-5 mb-sm-5 custom-container">
       <div className="row cc">
         <div className="col-12 col-md-12 col-lg-5">
           <div className="p-3 d-flex flex-column justify-content-evenly align-items-center first-column">
@@ -48,7 +72,7 @@ const UserPanel = () => {
               </div>
             )}
             {activeButton === "user-info" && (
-              <div className="user-content d-flex flex-column h-100 ">
+              <div className="user-content d-flex flex-column h-100">
                 <h1 className="user-name text-center py-4">Użytkownik</h1>
 
                 <p className="user-data user-data-name">E-mail: </p>
@@ -69,9 +93,44 @@ const UserPanel = () => {
                       Zapis do newslettera
                     </label>
                   </div>
+                </div>
+                <p className="delete-account mt-5 show-modal" onClick={openModal}>usuń konto</p>
+
+                {/* Modal*/}
+                <div className={`modal fade ${showModal ? "show d-block" : "d-none"}`} tabIndex="-1">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title">Przykro nam, że nas opuszczasz</h5>
+                        <button
+                          type="button"
+                          class="btn-close" onClick={closeModal}
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body">
+                        <p>Ta operacja jest nieodwracalna i stracisz wszystkie swoje zapisane głosowania. Czy na pewno chcesz kontynuować usuwanie konta?</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button
+                          type="button"
+                          class='btn btn-secondary' onClick={closeModal}
+                          data-bs-dismiss="modal"
+                        >
+                         Nie, zatrzymaj usuwanie
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                          Tak, usuń moje konto
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <p className="delete-account mt-5 ">usuń konto</p>
-                
+                </div>
+                {/* Overlay */}
+                {showModal && (
+                  <div className="modal-backdrop fade show" onClick={closeModal}></div>
+                )}
               </div>
             )}
             {activeButton === "settings" && (
